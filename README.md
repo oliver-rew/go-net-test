179dd524d5264fcbdc3421a97276174da63edb83
Oliver Rew <oliver.rew@rigado.com>
2020-08-14 15:30:16 -0700

    made basic tool for testing interfaces

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-net-net
+
+very simple tool for testing network interfaces
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func getDialerForInterface(intf string) (*net.Dialer, error) {
+	switch {
+	case strings.Contains(intf, "ppp"):
+		return getPPPDialer(intf), nil
+	default:
+		return getGenericDialer(intf)
+	}
+}
+
+func getGenericDialer(intf string) (*net.Dialer, error) {
+	ief, err := net.InterfaceByName(intf)
+	if err != nil {
+		log.Fatal(err)
+		return nil, fmt.Errorf("error getting '%s' interface by name: %s", intf, err)
+	}
+
+	addrs, err := ief.Addrs()
+	if err != nil {
+		return nil, fmt.Errorf("error getting '%s' addresses: %s", intf, err)
+	}
+
+	if len(addrs) < 1 {
+		return nil, fmt.Errorf("interface '%s' had no IPs", intf)
+	}
+
+	tcpAddr := &net.TCPAddr{
+		IP: addrs[0].(*net.IPNet).IP,
+	}
+
+	return &net.Dialer{
+		Timeout:   30 * time.Second,
+		LocalAddr: tcpAddr,
+	}, nil
+}
+
+func getPPPDialer(intf string) *net.Dialer {
+	return &net.Dialer{
+		Timeout: 30 * time.Second,
+		Control: func(network, address string, c syscall.RawConn) error {
+			return c.Control(func(fd uintptr) {
+				err := syscall.SetsockoptString(int(fd), syscall.SOL_SOCKET, 25, intf)
+				if err != nil {
+					log.Printf("control: %s", err)
+					return
+				}
+			})
+		}}
+}
+
+func main() {
+	// get the relevant args
+	intf := os.Args[1]
+	destAddr := os.Args[2]
+	destPort := os.Args[3]
+
+	// get the dialer based on the interface
+	d, err := getDialerForInterface(intf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := d.Dial("tcp", fmt.Sprintf("%s:%s", destAddr, destPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Successful connection with local IP '%s' on %s', to remote '%s:%s'\n", conn.LocalAddr().String(), intf, destAddr, destPort)
+
+	//	_, err = conn.Write([]byte("GET\n\n"))
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+
+	b, err := ioutil.ReadAll(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", b)
+}
